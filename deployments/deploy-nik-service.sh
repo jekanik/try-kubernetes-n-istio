ba38ca8ca71b7b342cbe081ac38f73cd1f2e8fa5
enikolskiy <enikolskiy@playtika.com>
2020-08-12 23:10:36 +0300

    Add ability to have placeholders in yaml configs Add service Fix awk usage

--- deployments/deploy-nik-service.sh
+++ deployments/deploy-nik-service.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 DEPLOYMENT="nik-deployment"
+REPLICAS=1
 
-kubectl apply -f $DEPLOYMENT.yaml
+vars=(REPLICAS)
+
+# replaces placeholders in yaml file. Name of variable must be equal to placeholder.
+replaceVars() {
+  local result=""
+  for var in ${vars[*]}; do
+    result+="s/{{${var}}}/${!var}/g; "
+  done
+
+  echo $result
+}
+
+sed "$(replaceVars)" $DEPLOYMENT.yaml | kubectl apply -f -
 
 READY=""
-while [ "$READY" != "1/1" ]; do
-  READY=$(kubectl get deployments | grep $DEPLOYMENT | awk '{print $2}')
+while [ "$READY" != "${REPLICAS}/${REPLICAS}" ]; do
+  READY=$(kubectl get deployments | awk /${DEPLOYMENT}'/{print $2}')
   echo "Ready: $READY"
   sleep 3s
 done
