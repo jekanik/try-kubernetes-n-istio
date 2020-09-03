a3f9d725c2ff10e3dde93c7d4e7818c37ce81272
enikolskiy <enikolskiy@playtika.com>
2020-09-03 18:00:33 +0300

    Expose prometheus-like metrics P.S.: default istio prometheus suit is querying pod by https and fails. TODO disable tls/find better solution.

--- service-two/src/main/java/com/jeka/nik/ServiceTwoApp.java
+++ service-two/src/main/java/com/jeka/nik/ServiceTwoApp.java
@@ -2,6 +2,8 @@ package com.jeka.nik;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class ServiceTwoApp {
 
+    private final DistributionSummary distribution;
+
+    public ServiceTwoApp(MeterRegistry meterRegistry) {
+        this.distribution = createDistribution(meterRegistry);
+    }
+
     @GetMapping("/")
     public String home() {
         return "Service Two, hello world!";
@@ -21,10 +29,17 @@ public class ServiceTwoApp {
     @GetMapping("/internal")
     public String internal() {
         long nextLong = ThreadLocalRandom.current().nextLong(1, 1000);
+        distribution.record(nextLong);
         return "ID: " + nextLong;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceTwoApp.class, args);
+    }
+
+    private static DistributionSummary createDistribution(MeterRegistry meterRegistry) {
+        return DistributionSummary
+                       .builder("service.two.internal.distribution")
+                       .register(meterRegistry);
     }
 }
